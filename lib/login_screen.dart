0a847afc39023384de0fecd2d03ea66be761ec3b
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-11 21:43:33 +0200

    handling pages routes

---
+++ lib/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+  static const String pageRoute = 'Login Screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
